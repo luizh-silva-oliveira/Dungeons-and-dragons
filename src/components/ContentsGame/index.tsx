b5a87a71b6f4a4a1f32a8818fefc8a9dfdab678e
Joao Victor Negreiros <joaovictornsv@gmail.com>
2022-10-01 10:07:02 -0300

    refactor: contents game list

--- src/components/ContentsGame/index.tsx
+++ src/components/ContentsGame/index.tsx
@@ -3,15 +3,40 @@ import styles from './styles.module.scss';
 
 import Witch from '../../assets/witcher.jpg';
 
+interface ListItemProps {
+  text: string;
+  href: string;
+}
+
+function ListItem({text, href}: ListItemProps) {
+  return (
+    <li>
+      <a href={href} target="_blank">
+        {text}
+      </a>
+    </li>
+  )
+}
+
+
 export function ContentsGame() {
   return (
     <Background urlImage={Witch} direction='center'>
       <div className={styles.Content}>
         <h1>Conteúdo D&D</h1>
         <ul>
-          <li> <a href="https://drive.google.com/file/d/1-1nq19rzFnmCsP9esNRh5IUKGrtQLWYz/view?usp=sharing" target="_blank">Livro do jogador - 5ª edição</a> </li>
-          <li> <a href="https://drive.google.com/file/d/1dgTOCNYybxMuD5x8IEC9z2Vsl7cy2S26/view?usp=sharing" target="_blank">Manual dos Monstros</a> </li>
-          <li> <a href="https://drive.google.com/file/d/1xYXm_pJc0RWTJ3mofG9LQuGfFSWmwjTL/view?usp=sharing" target="_blank">Caldeirão de Tasha para Tudo</a> </li>
+          <ListItem
+            text="Livro do jogador - 5ª edição"
+            href="https://drive.google.com/file/d/1-1nq19rzFnmCsP9esNRh5IUKGrtQLWYz/view?usp=sharing"
+          />
+          <ListItem
+            text="Manual dos Monstros"
+            href="https://drive.google.com/file/d/1dgTOCNYybxMuD5x8IEC9z2Vsl7cy2S26/view?usp=sharing"
+          />
+          <ListItem
+            text="Caldeirão de Tasha para Tudo"
+            href="https://drive.google.com/file/d/1xYXm_pJc0RWTJ3mofG9LQuGfFSWmwjTL/view?usp=sharing"
+          />
         </ul>
       </div>
 
